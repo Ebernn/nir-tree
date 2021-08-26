6e50017706a0d7a2dc5f72956979d9c0999510d9
Ebernn <r1.bernier@orange.fr>
2021-08-26 14:03:30 +0200

    Geometry intersections

--- src/utils/geometry.test.ts
+++ src/utils/geometry.test.ts
@@ -2,6 +2,10 @@ import {
   rectangleIncludesPoint,
   polygonIncludesPoint,
   rectanglesAreDisjoint,
+  rectangleVolume,
+  polygonVolume,
+  polygonIntersection,
+  rectangleIntersection,
 } from './geometry';
 import { Point, Rectangle } from './types';
 
@@ -46,44 +50,83 @@ describe('Geometry', () => {
       [5, 4],
     ];
 
-    it('should include points', () => {
-      const points: Point<2>[] = [
-        [2.25, 2.5],
-        [2.75, 3.5],
-      ];
-      expect(
-        points
-          .map((point) => rectangleIncludesPoint(rectangleA, point))
-          .every((v) => v === true)
-      ).toBe(true);
+    describe('properties', () => {
+      it('should deduce correclty their volume', () => {
+        expect(
+          rectangleVolume([
+            [0, 0],
+            [0, 10],
+          ])
+        ).toBe(0);
+        expect(
+          rectangleVolume([
+            [1, 3],
+            [3, 5],
+          ])
+        ).toBe(4);
+        expect(
+          rectangleVolume([
+            [0, 0, 0],
+            [10, 10, 0],
+          ])
+        ).toBe(0);
+        expect(
+          rectangleVolume([
+            [1, 3, 2],
+            [3, 5, 4],
+          ])
+        ).toBe(8);
+      });
     });
-    it('should include points (edge cases)', () => {
-      const points: Point<2>[] = [
-        [2, 2],
-        [3, 4],
-      ];
-      expect(
-        points
-          .map((point) => rectangleIncludesPoint(rectangleA, point))
-          .every((v) => v === true)
-      ).toBe(true);
+
+    describe('inclusions', () => {
+      it('should include points', () => {
+        const points: Point<2>[] = [
+          [2.25, 2.5],
+          [2.75, 3.5],
+        ];
+        expect(
+          points
+            .map((point) => rectangleIncludesPoint(rectangleA, point))
+            .every((v) => v === true)
+        ).toBe(true);
+      });
+      it('should include points (edge cases)', () => {
+        const points: Point<2>[] = [
+          [2, 2],
+          [3, 4],
+        ];
+        expect(
+          points
+            .map((point) => rectangleIncludesPoint(rectangleA, point))
+            .every((v) => v === true)
+        ).toBe(true);
+      });
+      it('should NOT include points', () => {
+        const points: Point<2>[] = [
+          [3.25, 2.5],
+          [2.75, 0.5],
+        ];
+        expect(
+          points
+            .map((point) => rectangleIncludesPoint(rectangleA, point))
+            .every((v) => v === false)
+        ).toBe(true);
+      });
     });
-    it('should NOT include points', () => {
-      const points: Point<2>[] = [
-        [3.25, 2.5],
-        [2.75, 0.5],
-      ];
-      expect(
-        points
-          .map((point) => rectangleIncludesPoint(rectangleA, point))
-          .every((v) => v === false)
-      ).toBe(true);
-    });
-    it('shoud be disjoint', () => {
-      expect(rectanglesAreDisjoint(rectangleA, rectangleC)).toBe(true);
-    });
-    it('shoud NOT be disjoint', () => {
-      expect(rectanglesAreDisjoint(rectangleA, rectangleB)).toBe(false);
+
+    describe('intersections', () => {
+      it('shoud be disjoint', () => {
+        expect(rectanglesAreDisjoint(rectangleA, rectangleC)).toBe(true);
+      });
+      it('shoud NOT be disjoint', () => {
+        expect(rectanglesAreDisjoint(rectangleA, rectangleB)).toBe(false);
+      });
+      it('should deduce correclty their intersections', () => {
+        expect(
+          JSON.stringify(rectangleIntersection(rectangleA, rectangleC))
+        ).toBe('[[0,0],[0,0]]');
+      });
     });
   });
   describe('Polygons', () => {
@@ -95,7 +138,7 @@ describe('Geometry', () => {
      *      + - - - - - -
      *          2 3
      */
-    const rectangleA: Rectangle<2> = [
+    const rectangle1A: Rectangle<2> = [
       [2, 2],
       [3, 4],
     ];
@@ -108,7 +151,7 @@ describe('Geometry', () => {
      *      + - - - - - -
      *            3 4
      */
-    const rectangleB: Rectangle<2> = [
+    const rectangle1B: Rectangle<2> = [
       [3, 2],
       [4, 3],
     ];
@@ -121,7 +164,7 @@ describe('Geometry', () => {
      *      + - - - - - -
      *              4 5
      */
-    const rectangleC: Rectangle<2> = [
+    const rectangle1C: Rectangle<2> = [
       [4, 2],
       [5, 4],
     ];
@@ -134,48 +177,134 @@ describe('Geometry', () => {
      *      + - - - - - -
      *          2 3 4 5
      */
-    const polygon = [rectangleA, rectangleB, rectangleC];
+    const polygon1 = [rectangle1A, rectangle1B, rectangle1C];
 
-    it('should include points', () => {
-      const points: Point<2>[] = [
-        [2.5, 2.5],
-        [3.5, 2.5],
-        [4.5, 2.5],
-        [2.5, 3.5],
-        [4.5, 3.5],
-      ];
-      expect(
-        points
-          .map((point) => polygonIncludesPoint(polygon, point))
-          .every((v) => v === true)
-      ).toBe(true);
+    /**
+     *      |
+     *  3   |     ┌── +
+     *  2   |     + ──┘
+     *      |
+     *      + - - - - - -
+     *            3   5
+     */
+    const rectangle2A: Rectangle<2> = [
+      [3, 2],
+      [5, 3],
+    ];
+
+    /**
+     *  4   |     ┌── +
+     *  3   |     + ──┘
+     *      |
+     *      |
+     *      + - - - - - -
+     *            3   5
+     */
+    const rectangle2B: Rectangle<2> = [
+      [3, 3],
+      [5, 4],
+    ];
+
+    /**
+     *  4   |     ┌───┐
+     *  3   |     ├───┤
+     *  2   |     └───┘
+     *      |
+     *      + - - - - - -
+     *            3 4 5
+     */
+    const polygon2 = [rectangle2A, rectangle2B];
+
+    describe('properties', () => {
+      it('should deduce correclty their volume', () => {
+        expect(polygonVolume(polygon1)).toBe(5);
+        expect(
+          polygonVolume([
+            [
+              [2, 2, 1],
+              [3, 4, 3],
+            ],
+            [
+              [3, 2, 2],
+              [4, 3, 3],
+            ],
+            [
+              [4, 2, 1],
+              [5, 4, 1],
+            ],
+          ])
+        ).toBe(5);
+      });
     });
-    it('should include points (edge cases)', () => {
-      const points: Point<2>[] = [
-        [2, 2],
-        [3, 3],
-        [4, 3],
-        [5, 4],
-      ];
-      expect(
-        points
-          .map((point) => polygonIncludesPoint(polygon, point))
-          .every((v) => v === true)
-      ).toBe(true);
+
+    describe('inclusions', () => {
+      it('should include points', () => {
+        const points: Point<2>[] = [
+          [2.5, 2.5],
+          [3.5, 2.5],
+          [4.5, 2.5],
+          [2.5, 3.5],
+          [4.5, 3.5],
+        ];
+        expect(
+          points
+            .map((point) => polygonIncludesPoint(polygon1, point))
+            .every((v) => v === true)
+        ).toBe(true);
+      });
+      it('should include points (edge cases)', () => {
+        const points: Point<2>[] = [
+          [2, 2],
+          [3, 3],
+          [4, 3],
+          [5, 4],
+        ];
+        expect(
+          points
+            .map((point) => polygonIncludesPoint(polygon1, point))
+            .every((v) => v === true)
+        ).toBe(true);
+      });
+      it('should NOT include points', () => {
+        const points: Point<2>[] = [
+          [1.5, 2.5],
+          [5.5, 2.5],
+          [1.5, 3.5],
+          [3.5, 3.5],
+          [5.5, 3.5],
+        ];
+        expect(
+          points
+            .map((point) => polygonIncludesPoint(polygon1, point))
+            .every((v) => v === false)
+        ).toBe(true);
+      });
     });
-    it('should NOT include points', () => {
-      const points: Point<2>[] = [
-        [1.5, 2.5],
-        [5.5, 2.5],
-        [1.5, 3.5],
-        [3.5, 3.5],
-        [5.5, 3.5],
-      ];
-      expect(
-        points
-          .map((point) => polygonIncludesPoint(polygon, point))
-          .every((v) => v === false)
-      ).toBe(true);
+
+    describe('intersections', () => {
+      it('should deduce correclty their intersections', () => {
+        expect(JSON.stringify(polygonIntersection(polygon1, polygon2))).toBe(
+          '[[[3,2],[4,3]],[[4,2],[5,3]],[[4,3],[5,4]]]'
+        );
+        expect(
+          JSON.stringify(
+            polygonIntersection(polygon1, [
+              [
+                [2, 2],
+                [5, 4],
+              ],
+            ])
+          )
+        ).toBe(JSON.stringify(polygon1));
+        expect(
+          polygonIntersection(polygon1, [
+            [
+              [2, 0],
+              [2, 5],
+            ],
+          ]).length
+        ).toBe(0);
+      });
     });
   });
 });
--- src/utils/geometry.ts
+++ src/utils/geometry.ts
@@ -7,13 +7,12 @@ import { Point, Polygon, Rectangle } from './types';
  * @returns {boolean} true if the rectangle includes the point, false otherwise
  */
 export const rectangleIncludesPoint = <Dimensions extends number>(
-  rectangle: Rectangle<Dimensions>,
+  [point0, point1]: Rectangle<Dimensions>,
   point: Point<Dimensions>
 ): boolean => {
   const dimensions = point.length;
   for (let axis = 0; axis < dimensions; axis++) {
-    if (rectangle[0][axis] > point[axis] || rectangle[1][axis] < point[axis])
-      return false;
+    if (point0[axis] > point[axis] || point1[axis] < point[axis]) return false;
   }
   return true;
 };
@@ -38,17 +37,15 @@ export const polygonIncludesPoint = <Dimensions extends number>(
  * Check if two rectangles are disjoint a point or not.
  * @param rectangleA
  * @param rectangleB
- * @returns true if their intersection is empty, false otherwise
+ * @returns {boolean} true if their intersection is empty, false otherwise
  */
 export const rectanglesAreDisjoint = <Dimensions extends number>(
-  rectangleA: Rectangle<Dimensions>,
+  [pointA0, pointA1]: Rectangle<Dimensions>,
   rectangleB: Rectangle<Dimensions>
 ): boolean => {
-  const rectangleACorners: Point<Dimensions>[] = rectangleA[1].map<
-    Point<Dimensions>
-  >(
+  const rectangleACorners: Point<Dimensions>[] = pointA1.map<Point<Dimensions>>(
     (component1, i) =>
-      rectangleA[0].map((component0, j) =>
+      pointA0.map((component0, j) =>
         i === j ? component1 : component0
       ) as Point<Dimensions>
   );
@@ -57,3 +54,74 @@ export const rectanglesAreDisjoint = <Dimensions extends number>(
   }
   return true;
 };
+
+/**
+ * Returns a rectangle volume.
+ * @param rectangle
+ * @returns {number} volume
+ */
+export const rectangleVolume = <Dimensions extends number>([
+  point0,
+  point1,
+]: Rectangle<Dimensions>): number =>
+  point1
+    .map((component, axis) => component - point0[axis])
+    .reduce((accVolume, curVolume) => accVolume * curVolume);
+
+/**
+ * Returns a polygon volume.
+ * @param rectangle
+ * @returns {number} volume
+ */
+export const polygonVolume = <Dimensions extends number>(
+  rectangles: Polygon<Dimensions>
+): number =>
+  rectangles
+    .map((rectangle) => rectangleVolume(rectangle))
+    .reduce((accVolume, curVolume) => accVolume + curVolume);
+
+/**
+ * Returns the intersection of two rectangles.
+ * @param rectangleA
+ * @param rectangleB
+ * @returns the intersection of these two rectangles
+ */
+export const rectangleIntersection = <Dimensions extends number>(
+  [A0, A1]: Rectangle<Dimensions>,
+  [B0, B1]: Rectangle<Dimensions>
+): Rectangle<Dimensions> =>
+  rectanglesAreDisjoint([A0, A1], [B0, B1])
+    ? [
+        new Array(A0.length).fill(0) as Point<Dimensions>,
+        new Array(A0.length).fill(0) as Point<Dimensions>,
+      ]
+    : [
+        new Array(A0.length)
+          .fill(0)
+          .map((_, axis) => Math.max(A0[axis], B0[axis])) as Point<Dimensions>,
+        new Array(A0.length)
+          .fill(0)
+          .map((_, axis) => Math.min(A1[axis], B1[axis])) as Point<Dimensions>,
+      ];
+
+/**
+ * Returns the intersection of two polygons.
+ * @param polygonA
+ * @param polygonB
+ * @returns the intersection of these two polygons
+ */
+export const polygonIntersection = <Dimensions extends number>(
+  rectanglesA: Polygon<Dimensions>,
+  rectanglesB: Polygon<Dimensions>
+): Polygon<Dimensions> =>
+  rectanglesA
+    .map((rectangle1) =>
+      // TODO : improve this part, quadratic complexity for now :S
+      rectanglesB
+        // get their intersection
+        .map((rectangle2) => rectangleIntersection(rectangle1, rectangle2))
+    )
+    // concat them all
+    .reduce((acc, cur) => [...acc, ...cur])
+    // remove empty rectangles
+    .filter((rectangle) => rectangleVolume(rectangle) > 0);
